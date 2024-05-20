cf0319216e7449e977e20d5e8f5e675dcb11356f
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-05-20 16:10:45 +0300

    Update the sequences file

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # initialize an empty list
+    fibonacci_sequence = []
+    if length > 0:
+        fibonacci_sequence.append(0)
+    if length > 1:
+        fibonacci_sequence.append(1)
+    while len(fibonacci_sequence) < length:
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+
+    print(fibonacci_sequence)        
+
